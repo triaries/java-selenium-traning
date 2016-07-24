39fed2475468db09aeef2c5db9c0fc4c7ce3e821
Kubrakov Sergey <triaries@gmail.com>
2016-07-24 16:58:48 +0300

    First Test

---
+++ src/test/java/ua/stqa/selenium/LoginTest.java
@@ -0,0 +1,23 @@
+package ua.stqa.selenium;
+
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+
+
+public class LoginTest extends TestNgTestBase{
+
+
+  @Test
+  public void testLogin() throws Exception {
+    driver.get(baseUrl + "/php4dvd/");
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("submit")).click();
+  }
+}
+
+
+
